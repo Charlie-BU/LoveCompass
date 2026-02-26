e32ee80d7dd949e3528de6dfed91553d8a35f7bf
Charlie BU <butian.charlie@bytedance.com>
2026-02-26 17:17:37 +0800

    chores: ditto

--- BE-HeartCompass/server/services/ai.py
+++ BE-HeartCompass/server/services/ai.py
@@ -1,3 +1,4 @@
+import os
 from agent.react_agent import getAgent, askWithNoContext
 from agent.prompt import getPrompt
 
@@ -5,7 +6,7 @@ from agent.prompt import getPrompt
 # 对上下文记录或知识库条目进行摘要
 async def summarizeContext(content: str) -> str:
     prompt = await getPrompt(
-        "https://www.prompt-minder.com/share/58962157-c1a3-4c6c-a37c-b151de2bc5e1",
+        os.getenv("SUMMARIZE_CONTENT_PROMPT"),
         {"content": content},
     )
     print(prompt)
@@ -19,7 +20,7 @@ async def summarizeContext(content: str) -> str:
 # 将自然语言组织拆分与提炼为knowledge
 async def extractKnowledge(content: str) -> str:
     prompt = await getPrompt(
-        "https://www.prompt-minder.com/share/ebb2e5e8-3ea9-4122-b272-74cdd74d68cb",
+        os.getenv("EXTRACT_KNOWLEDGE_PROMPT"),
         {"content": content},
     )
     agent = await getAgent()
@@ -32,7 +33,7 @@ async def extractKnowledge(content: str) -> str:
 # 将自然语言的信息转为 crush_profile 和 event
 async def normalizeContext(content: str) -> str:
     prompt = await getPrompt(
-        "https://www.prompt-minder.com/share/8e36f272-7f64-4210-b511-a9ee6b47bf00",
+        os.getenv("NORMALIZE_CONTEXT_PROMPT"),
         {"content": content},
     )
     agent = await getAgent()
